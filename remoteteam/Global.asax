8b11f9f4b0a047eff3017fd5aa016e2a21680509
fray <francis.yanga@foundationfootprint.com>
2018-05-30 22:48:48 +1200

    initial

---
+++ remoteteam/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="remoteteam.MvcApplication" Language="C#" %>
---
+++ remoteteam/Hubs/MessageHub.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNet.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace remoteteam.Hubs
+{
+    public class MessageHub : Hub
+    {
+        public void Send(string clientId, string message)
+        {
+            Clients.All.addNewMessageToPage(clientId, message);
+        }
+    }
+}
